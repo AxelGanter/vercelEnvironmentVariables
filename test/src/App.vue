d92059515987cd9bb0eccf15f99e88c1b1292c03
Axel Ganter <mail@axelganter.de>
2020-04-24 09:46:10 +0200

    vuecli-demo

---
+++ test/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <h1>process.env</h1>
+    <hr><p>{{envVariables}}</p><hr>
+  </div>
+</template>
+
+<script>
+
+export default {
+  data(){
+    return{
+      envVariables: process.env
+    }
+  }
+}
+
+</script>
